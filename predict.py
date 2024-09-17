b374863d3941d25cb4952714508e300c580400e5
Azmeer Faisal <92108835+azmeer36@users.noreply.github.com>
2024-09-17 08:09:04 +0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,6 +1,8 @@
 from flask_smorest import Blueprint
 from flask import request, jsonify, Response
 from model import mock_model_predict
+from schemas import PredictionInputSchema, PredictionResponseSchema, AsyncResponseSchema, ErrorResponseSchema, AsyncPredictionSchema
+from schemas import AsyncModeHeaderSchema
 import threading
 import uuid
 import queue
@@ -10,8 +12,6 @@ blp = Blueprint("predict", __name__, description="Prediction operations")
 # Initialize task queue and predictions dictionary
 task_queue = queue.Queue() # For lining up async tasks
 predictions = {} # Temporary storage for async prediction results
-
-
 
 # Background worker thread
 def worker() -> None:
@@ -26,28 +26,19 @@ def worker() -> None:
 # Start worker thread
 threading.Thread(target=worker, daemon=True).start()
 
+
 @blp.route("/predict", methods=["POST"])
-@blp.response(200, description="Returns the prediction results or ID for async requests")
-def predict() -> Response:
-    
-    # Perform prediction on synchronous or asynchronous requests.
-    
-    data = request.get_json()
-    if data is None: # Return Error if not in JSON format
-        return jsonify({"error": "Content Type is not JSON"}), 404
-
-    if 'input' not in data : # Return error if input not included
-        return jsonify({"error": "No input data provided"}), 400
-
-
-    if not isinstance(data['input'], str):
-        return jsonify({"error": "Input data should be string"}), 405
-        
-    input_data = data['input']
+@blp.arguments(PredictionInputSchema, location="json")
+@blp.arguments(AsyncModeHeaderSchema, location="headers")
+@blp.response(200, PredictionResponseSchema, description="Synchronous Request Response")
+@blp.response(202, AsyncResponseSchema, description="Async Request Response ")
+def predict(args, async_mode) -> Response:
+    # Perform prediction on synchronous or asynchronous requests. 
+    input_data = args['input']
 
     async_mode = request.headers.get('Async-Mode')
     # Asynchronous prediction
-    if async_mode:
+    if async_mode == 'true' or async_mode == 'True':
         prediction_id = str(uuid.uuid4())  # Generate unique ID  # No need to check against prev generated IDs
         
         predictions[prediction_id] = None  # Reserve the ID
@@ -65,11 +56,12 @@ def predict() -> Response:
 
 
 @blp.route("/predict/<string:prediction_id>", methods=["GET"])
-@blp.response(200, description="Fetches the prediction results for async requests")
+@blp.response(200, AsyncPredictionSchema)
+@blp.response(400, ErrorResponseSchema)
+@blp.response(404, ErrorResponseSchema)
 def get_prediction_result(prediction_id: str) -> Response:
     
     # Fetch results of asynchronous predictions 
-    
     if prediction_id not in predictions: # Pred ID is incorrect
         return jsonify({"error": "Prediction ID not found"}), 404 
 
